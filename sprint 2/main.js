d8ad59cd54bf82d4895b907548d65fcaf3c17020
rshawer <33186139+rshawer@users.noreply.github.com>
2018-10-08 01:26:22 -0500

    print 2

--- sprint 2/main.js
+++ sprint 2/main.js
@@ -4,4 +4,7 @@ game.state.add('state1', st1);
 game.state.add('state2', st2);
 game.state.add('state3', st3);
 game.state.add('state4', st4);
-game.state.start('state4')
+game.state.add('state_tutorial', st_tut);
+//game.state.start('state4')
+// CHANGE BACK WHEN READY game.state.start('state0')
+game.state.start('state_tutorial');
