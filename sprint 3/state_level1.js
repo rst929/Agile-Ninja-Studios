fdf3a8510fc54f8e12f22c5979197852816343f6
Randy Steinkamp <randy118@utexas.edu>
2018-10-14 20:15:03 -0500

    Fixed platform, needs jump

--- sprint 3/state_level1.js
+++ sprint 3/state_level1.js
@@ -1,7 +1,8 @@
 var st_lev1 = {
     preload: p1,
     create: c1,
-    update: u1
+    update: u1,
+    render: r1
 }
 
 function p1() {
@@ -21,6 +22,96 @@ function p1() {
     game.load.tilemap('castle_map','assets/tilemap/castle.json',null, Phaser.Tilemap.TILED_JSON);
     game.load.image('stone_tile', 'assets/tilemap/stone_tile2.png');
     game.load.image('castle_tile', 'assets/tilemap/castle_background_v2.png');
+    
+    game.load.spritesheet('swordsman', 'assets/green_enemy.png', 285, 325);
+    this.load.text('enemySpawnLoc', 'assets/EnemySpawn.json');
+}
+
+EnemySwordsman = function(index, game, x, y) {
+
+    this.swordsman = game.add.sprite(x, y, 'swordsman');
+    this.swordsman.anchor.setTo(.5, .5);
+    this.swordsman.scale.setTo(.45,.45);
+    
+    //  We need to enable physics on the player
+    game.physics.arcade.enable(this.swordsman);
+    
+//    //create timer
+//    var attackTimer;
+//    attackTimer = game.time.create(false);
+//    attackTimer.loop(2000 + game.rnd.integerInRange(100, 200), console.log(2000 + game.rnd.integerInRange(100, 200)), this);
+    
+    this.swordsman.body.bounce.y = 0.2;
+    this.swordsman.body.gravity.y = 1000;
+    this.swordsman.body.collideWorldBounds = true;
+    //this.swordsman.body.setSize(15, 40, 0, 100);
+    
+    //animations
+    this.swordsman.animations.add('left', [8, 9], 3, true);
+    this.swordsman.animations.add('right', [6, 7], 3, true);
+    var attackL = this.swordsman.animations.add('attackL', [0, 1, 2, 2, 1, 0], 10, false);
+    var attackR = this.swordsman.animations.add('attackR', [3, 4, 5, 5, 4, 3], 10, false);
+    
+    
+    this.stand = function() {
+        this.swordsman.frame = 8;
+    };
+    
+    
+    var canAttack = false;
+    var lookingL = false;
+    var lookingR = false;
+    var extraDist = game.rnd.integerInRange(0, 10);
+    var velocity = game.rnd.integerInRange(200 , 300);
+    
+    this.canAttack = function() {
+        canAttack = true;
+    };
+    
+    game.time.events.loop(Phaser.Timer.SECOND * (1 +  game.rnd.integerInRange(1, 2)), this.canAttack, this);
+    
+    this.move = function(pX) {
+        console.log(canAttack);
+        if(pX + 100 + extraDist <= this.swordsman.x) { //go left
+            this.swordsman.body.velocity.x = -velocity;
+            this.swordsman.animations.play('left');
+            lookingL = true;
+            lookingR = false;
+//            attackTimer.stop(false);
+            
+        } else if (pX - extraDist >= this.swordsman.x) { //go right
+            this.swordsman.body.velocity.x = velocity;
+            this.swordsman.animations.play('right');
+            lookingL = false;
+            lookingR = true;
+//            attackTimer.stop(false);
+        } else if(lookingL) {
+            this.swordsman.body.velocity.x = 0;
+            if(canAttack) {
+                this.swordsman.play('attackL');
+                canAttack = false;
+            }
+            
+        } else if(lookingR) {
+            this.swordsman.body.velocity.x = 0;
+            if(canAttack) {
+                this.swordsman.play('attackR');
+                canAttack = false;
+            }
+        } else {
+            this.swordsman.body.velocity.x = 0;
+            this.swordsman.frame = 8;
+        }
+    }
+    
+    //if swordsman is dead
+    this.die = function() {
+        //isDead = true;
+        //swordsman.animations.play('dead');
+        //swordsman.animations.currentAnim.onComplete.destroy();
+        swordsman.destroy();
+    };
+    //if()
 }
 
 var image; //background
@@ -32,51 +123,37 @@ var instructions; //game instructions'
 var stone_platforms;
 //var background;
 
+var map;
+var stone_platforms;
+var background;
+
+
 function c1() {
     
-    game.world.setBounds(0, 0, 2400, 416);
+    //game.world.setBounds(0, 0, 2400, 416);
     //  Physics
     game.physics.startSystem(Phaser.Physics.ARCADE);
-    //image = game.add.sprite(0, 0, 'castle'); // first visible bkgd
-    //image.width = game.width;
-    //image.height = game.height + 100;
-    //game.physics.enable(image, Phaser.Physics.ARCADE); 
-    
+
     //creating map
-    var map = game.add.tilemap('castle_map');
-    map.addTilesetImage('stone_tile2','stone_tile');
+    map = game.add.tilemap('castle_map');
+    map.addTilesetImage('stone_tile2','stone_tile')
     map.addTilesetImage('castle_background','castle_tile');
-    var background = map.createLayer('Background');
-    var stone_platforms = map.createLayer('Platforms');
-    //game.physics.enable(map, Phaser.Physics.ARCADE);    //creating layers
-    game.physics.enable(stone_platforms, Phaser.Physics.ARCADE);    //creating layers
-    //game.physics.enable(background, Phaser.Physics.ARCADE);    //creating layers
+    
+    //layers
+    background = map.createLayer('Background');
+    stone_platforms = map.createLayer('Platforms');
+    //map.setTileIndexCallback(26, loggedVal, this);
+    
+    map.setCollisionBetween(1, 300, true, 'Platforms');    
     map.width=game.width;
     map.height=game.height+100;
     background.resizeWorld();
     stone_platforms.resizeWorld();
-    //background.wrap=true;
-    //stone_platforms.wrap=true;
-    // create platform for ground
-    //platforms = game.add.group();
-    //platforms.enableBody = true;
-    //var ground = platforms.create(0, game.world.height - 150, 'ground');
-    //ground.scale.setTo(2,2); //make ground right size
-    //  Make ground stable
-    //ground.body.immovable = true;
-    
-    //background.body.immovable=true;
-    //background.enableBody=true;
-    
-    stone_platforms.enableBody=true;
-    stone_platforms.body.immovable= true;
+    stone_platforms.debug = true;
     
     // The player and its settings
     player = game.add.sprite(350, game.world.height - 500, 'sam');
     player.scale.setTo(.6,.6)
-    
-    
-    game.physics.arcade.enable(stone_platforms);
     
     //  We need to enable physics on the player
     game.physics.arcade.enable(player);
@@ -86,8 +163,6 @@ function c1() {
     player.body.gravity.y = 1000;
     player.body.collideWorldBounds = true;
     player.body.setSize(15, 40, 0, 100);
-
-    map.setCollision([1,229],true,stone_platforms);
     
     //  animations
     player.animations.add('left', [0, 1], 10, true);
@@ -107,25 +182,38 @@ function c1() {
     //camerma moves
     
     game.camera.follow(player);
+    
+    //setting up JSON file to be read
+    this.enemyLocData = JSON.parse(this.game.cache.getText('enemySpawnLoc'));
 
 }
 
 var pHealth = 100; //player health
 var playerVulnerable = true; //if player is vulnerable (out of 'i frames')
-
+var enemyLocIndex = 0;
+var swordsmanArray = [];
+var hitPlatform = false;
 
 function u1() {
     //  Collide the player and the stars with the platforms
-    var hitPlatform = game.physics.arcade.collide(player, stone_platforms, function(){console.log('hitting platforms!');}); //collide with platform (i.e. ground) check
-
+    game.physics.arcade.collide(player, stone_platforms, function(){hitPlatform = true}); //collide with platform (i.e. ground) check
+    console.log(hitPlatform);
     game.physics.arcade.TILE_BIAS = 40;
     game.physics.arcade.collide(player, stone_platforms);
+    //game.physics.arcade.collide(player, map);
     
     //movement tree for player
     if (cursors.left.isDown) {
         movePLeft();
     } else if (cursors.right.isDown) {
         movePRight();
+        if(this.enemyLocData.enemySpawnLoc[enemyLocIndex].x != -1) { //spawning enemies, check for array bounds
+            if(player.x == this.enemyLocData.enemySpawnLoc[enemyLocIndex].x) {
+                swordsmanArray.push(new EnemySwordsman(enemyLocIndex, game, player.x + 20, player.y));
+                //swordsman.stand();f
+                enemyLocIndex++;
+            }
+        }
     } else if(attackButton.isDown) {
         player.animations.play('attack');
     }
@@ -142,9 +230,11 @@ function u1() {
         player.body.velocity.x = 0;
     }
 
+    //note: removing player.body.touching.down allows player to jump, but means player can jump when alongside walls
     //  Allow the player to jump if they are touching the ground.
     if (cursors.up.isDown && player.body.touching.down && hitPlatform) {
         player.body.velocity.y = -700;
+        hitPlatform = false;
     }
     playerHealth.text = "Sam HP: " + pHealth;
     
@@ -162,6 +252,11 @@ function u1() {
 
 //note: some functions are small, but are as functions with the idea that more will be added to them later
 
+function r1() {
+    game.debug.body(stone_platforms);
+    game.debug.body(player);
+    
+}
 function swordAttack() {
     player.animations.play('attack');
 }
