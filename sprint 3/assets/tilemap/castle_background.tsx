65bc9a2fdc1c3adf9d3fab6321bc781f8b109c08
Randy Steinkamp <randy118@utexas.edu>
2018-11-30 11:27:09 -0600

    Trimmed branch temporarily

--- sprint 3/assets/tilemap/castle_background.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.0" name="castle_background" tilewidth="32" tileheight="32" tilecount="234" columns="18">
- <image source="castle_background_v2.png" width="592" height="426"/>
-</tileset>
